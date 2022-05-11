8e01c10ef117cd2f23d86fd88b9606e5e1b7eedb
dragonmux <git@dragonmux.network>
2022-05-10 20:46:08 -0400

    Added environment settings for debug info on Arch and SSH

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -7,5 +7,9 @@ export GTK_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
 export QT_IM_MODULE=ibus
 
+export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
+
+export DEBUGINFOD_URLS="https://debuginfod.archlinux.org/"
+
 # Stop the pager from always taking over (eg, in Git)
 unset LESS
